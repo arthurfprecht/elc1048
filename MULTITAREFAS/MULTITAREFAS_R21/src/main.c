14c45e896ee1c04caedeaf41d2074a981044783d
arthurfprecht <arthurfprecht@gmail.com>
2022-05-19 13:40:22 -0300

    mensagem para o commit funcionar

--- MULTITAREFAS/MULTITAREFAS_R21/src/main.c
+++ MULTITAREFAS/MULTITAREFAS_R21/src/main.c
@@ -40,6 +40,7 @@ void tarefa_5(void);
 void tarefa_6(void);
 void tarefa_7(void);
 void tarefa_8(void);
+void tarefa_nova(void);
 
 /*
  * Configuracao dos tamanhos das pilhas
@@ -54,6 +55,8 @@ void tarefa_8(void);
 #define TAM_PILHA_8			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_OCIOSA	(TAM_MINIMO_PILHA + 24)
 
+#define TAM_PILHA_NOVA			(TAM_MINIMO_PILHA + 24)
+
 /*
  * Declaracao das pilhas das tarefas
  */
@@ -67,6 +70,8 @@ uint32_t PILHA_TAREFA_7[TAM_PILHA_7];
 uint32_t PILHA_TAREFA_8[TAM_PILHA_8];
 uint32_t PILHA_TAREFA_OCIOSA[TAM_PILHA_OCIOSA];
 
+uint32_t PILHA_TAREFA_NOVA[TAM_PILHA_NOVA];
+
 /*
  * Funcao principal de entrada do sistema
  */
@@ -77,9 +82,11 @@ int main(void)
 	/* Criacao das tarefas */
 	/* Parametros: ponteiro, nome, ponteiro da pilha, tamanho da pilha, prioridade da tarefa */
 	
-	CriaTarefa(tarefa_1, "Tarefa 1", PILHA_TAREFA_1, TAM_PILHA_1, 1);
+	CriaTarefa(tarefa_1, "Tarefa 1", PILHA_TAREFA_1, TAM_PILHA_1, 2);
 	
-	CriaTarefa(tarefa_2, "Tarefa 2", PILHA_TAREFA_2, TAM_PILHA_2, 2);
+	CriaTarefa(tarefa_2, "Tarefa 2", PILHA_TAREFA_2, TAM_PILHA_2, 1);
+	
+	CriaTarefa(tarefa_nova, "Tarefa nova", PILHA_TAREFA_2, TAM_PILHA_NOVA, 1);
 	
 	/* Cria tarefa ociosa do sistema */
 	CriaTarefa(tarefa_ociosa,"Tarefa ociosa", PILHA_TAREFA_OCIOSA, TAM_PILHA_OCIOSA, 0);
@@ -243,5 +250,22 @@ void tarefa_8(void)
 		(void)valor;	/* leitura da variável para evitar aviso (warning) do compilador */
 		
 		SemaforoLibera(&SemaforoVazio);
+	}
+}
+
+
+
+void tarefa_nova(void)
+{
+	volatile uint16_t qualquer = 0;
+	for(;;)
+	{
+		qualquer++; /* É necessário incrementar alguma variável dentro do loop para o compilador não removê-lo */
+		
+		port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE);
+		TarefaEspera(100); 	/* Liga o led rapidamente */
+		
+		port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE);
+		TarefaEspera(100); 	/*  Desliga o led rapidamente */
 	}
 }
